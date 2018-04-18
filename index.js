c09bec72e33ca1fb9c0789f55713a196049508b5
Alexander Holovin <dvjmixxxer@gmail.com>
2018-04-18 04:43:40 +0300

    v1: alpha (still didnt tested!)
    * question/answers parser
    * loop implement (need rework)

--- index.js
+++ index.js
@@ -1,73 +1,89 @@
 const dotenv = require('dotenv').config();
-const req = require('request-promise');
-const moment = require('moment-timezone');
-const get = require('lodash/get');
 
+const request = require('request-promise-native');
+
+const req = request.defaults({
+    headers: {
+        'User-Agent': process.env.ua,
+    }
+});
+
+const to = require('await-to-js').to;
+const url = require('url');
+
+const moment = require('moment-timezone');
 moment.tz.setDefault('Europe/Minsk');
 moment.locale('ru');
 
+const get = require('lodash/get');
+const trimEnd = require('lodash/trimEnd');
+
+// helpers
+const checkApiError = require('./helpers/checkApiError');
+const runAfter = require('./helpers/runAfter');
+const processTimestamp = require('./helpers/processTimestamp');
 // --- //
 
 async function getStart() {
-    try {
-        const answer = await req.post({
-            url: process.env.URL_API_START,
-            json: true,
-        }).form({
-            build_ver: process.env.API_BUILD_VER,
-            need_leaderboard: process.env.API_NEED_LEADERBOARD,
-            func_v: process.env.API_FUNC_V,
-            access_token: process.env.API_ACCESS_TOKEN,
-            v: process.env.API_V,
-            lang: process.env.API_LANG,
-            https: process.env.API_HTTPS,
-        });
+    const [err, response] = await to(req.post({
+        url: process.env.URL_API_START,
+        json: true,
 
-        return {
-            gameId: get(answer, 'response.game_info.game.game_id', ''),
-            gameStatus: get(answer, 'response.game_info.game.status', ''),
-            startTime: processTimestamp(get(answer, 'response.game_info.game.start_time', ''), true),
-            server_time: processTimestamp(get(answer, 'response.server_time', '')),
-            prize: get(answer, 'response.game_info.game.prize', ''),
-            videoOwner: get(answer, 'response.game.video_owner_id', ''),
-            videoId: get(answer, 'response.game.video_id', ''),
-        };
-    } catch (e) {
-        console.warn(e);
-        return '';
-    }
-}
+    }).form({
+        build_ver: process.env.API_BUILD_VER,
+        need_leaderboard: process.env.API_NEED_LEADERBOARD,
+        func_v: process.env.API_FUNC_V,
+        access_token: process.env.API_ACCESS_TOKEN,
+        v: process.env.API_V,
+        lang: process.env.API_LANG,
+        https: process.env.API_HTTPS,
 
-function processTimestamp(timestampInput, addFrom = false) {
-    const timestamp = timestampInput * 1000;
-    const timestampStr = `${moment(timestamp).format('DD/MM/YYYY HH:mm:ss')}`;
+    }));
 
-    if (addFrom) {
-        return `${timestampStr} (через ${moment(timestamp).fromNow()})`;
+    if (err || checkApiError(response)) {
+        console.warn(err);
+        return;
     }
 
-    return timestampStr;
+    return {
+        gameId: get(response, 'response.game_info.game.game_id', ''),
+        gameStatus: get(response, 'response.game_info.game.status', ''),
+        startTime: processTimestamp(get(response, 'response.game_info.game.start_time', ''), true),
+        server_time: processTimestamp(get(response, 'response.server_time', '')),
+        prize: get(response, 'response.game_info.game.prize', ''),
+        videoOwner: get(response, 'response.game_info.game.video_owner_id', ''),
+        videoId: get(response, 'response.game_info.game.video_id', ''),
+    };
 }
 
-// INIT
+// Init
 (async () => {
     await gameWaiter();
 })();
 
-//
+// Core function
 async function gameWaiter() {
     const status = await getStart();
     console.warn(`WAIT: `, status);
 
     // started!
     if (status.gameStatus === 'started') {
-        const lp_url = await getLongPollUrl(status.videoOwner, status.videoId);
+        const lp_url_raw = await getLongPollUrl(status.videoOwner, status.videoId);
+        const url_params = url.parse(lp_url_raw, true);
+
+        // [?param1=value1] part, need for increment ts_id
+        const lp_params = url_params.query;
+
+        // [https://....?] part
+        const lp_url = `${url_params.protocol}//${url_params.host}`;
+
+        if (lp_url) {
+            await getNextEvent(lp_url, lp_params);
+        }
 
     } else if (status.gameStatus === 'planned') {
         // do next
-        setTimeout(async () => {
-            await gameWaiter();
-        }, 5000);
+        await runAfter(gameWaiter, [], 5000);
 
     } else {
         // something wrong?
@@ -76,9 +92,10 @@ async function gameWaiter() {
 }
 
 async function getLongPollUrl(videoOwner, videoId) {
-    const answer = await req.post({
+    const [err, response] = await to(req.post({
         url: process.env.URL_API_GET_LP,
         json: true,
+
     }).form({
         video_id: videoId,
         owner_id: videoOwner,
@@ -86,7 +103,73 @@ async function getLongPollUrl(videoOwner, videoId) {
         v: process.env.API_V,
         lang: process.env.API_LANG,
         https: process.env.API_HTTPS,
-    });
+    }));
 
-    return answer.response.url;
+    if (err || checkApiError(response)) {
+        console.warn(err);
+        return;
+    }
+
+    console.log(response);
+
+    return response.response.url;
+}
+
+async function getNextEvent(lp_url, lp_params) {
+    const [err, response] = await to(req.get({
+        url: lp_url,
+        qs: lp_params,
+        json: true,
+    }));
+
+    if (err || checkApiError(response)) {
+        console.warn(err);
+
+        runAfter(getNextEvent, [lp_url, lp_params], 300);
+        return false;
+    }
+
+    // update TS
+    lp_params.ts = response.ts;
+
+    // seems ok?
+    if (response.events.length > 0) {
+
+        // wtf is <!>0?
+        response.events.forEach(rawEvent => {
+            const event = JSON.parse(trimEnd(response.events[0], '<!>0'));
+
+            // take question ->
+            if (event.type === 'sq_question' || event.type === 'sq_question_answers_right') {
+                const question = get(event, 'question.text', '');
+                const number = get(event, 'question.number', '');
+                const answers = [];
+
+                if (event.type === 'sq_question') {
+                    get(event, 'question.answers', []).forEach(answer => {
+                        answers.push(answer.text);
+                    });
+
+                } else {
+                    get(event, 'question.answers', []).forEach(answer => {
+                        answers.push(`${answer.text} (${answer.users_answered})`);
+                    });
+                }
+
+                // TODO: rework after tests
+                console.log(`${number}. ${question}\n > ${answers.join('\n > ')}`);
+            }
+
+            // ITS TIME TO STOP, OKAY?
+            if (event.type === 'sq_ed_game') {
+                console.warn('!!! Receive stop event !!!');
+                console.warn(event);
+
+                return true;
+            }
+        });
+    }
+
+    // get next events
+    await runAfter(getNextEvent, [lp_url, lp_params], 300);
 }
